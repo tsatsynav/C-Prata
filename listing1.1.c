e58fb68afa5e7518ac4f404823daa0775f077810
Andrey Tsatsyn <tsatsynav@yandex.ru>
2024-07-23 20:54:07 +0300

    added listing 2.2

--- listing1.1.c
+++ listing1.1.c
@@ -1,3 +1,12 @@
+// C Primer Plus (6th edition). Stephen Prata. Listing 1.1.
+// Fedora Linux 40 (Workstation Edition).
+// gcc (GCC) 14.1.1 20240701 (Red Hat 14.1.1-7)
+// Lenovo ThinkPad E15 Gen 4
+// 12th Gen Intel® Core™ i5-1235U × 12
+// Andrey Tsatsyn
+// tsatsynav@yandex.ru
+// https://t.me/TsatsynAV
+
 #include <stdio.h>
 
 int main(void) {
